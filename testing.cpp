3eb72caabd4caa649705ae04358fae63fb8725e3
joshdewhurst <joshuardewhurst@gmail.com>
2022-10-25 12:16:08 -0700

    testing creating variables

---
+++ testing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// creating a namespace to track libraries
+using std::cout;
+
+int main() {
+    // creating a data type string
+  std::string name = "Gods Name";
+//   console out the name variable
+  cout << name;
+}

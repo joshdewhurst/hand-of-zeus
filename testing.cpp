57fa2fbb65e4218dd8fbb90b7d4b182f7373da5b
joshdewhurst <joshuardewhurst@gmail.com>
2022-10-26 10:00:00 -0700

    data types in testing

--- testing.cpp
+++ testing.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 
 // creating a namespace to track libraries
 using std::cout;
@@ -7,10 +8,25 @@ using std::string;
 
 int main() {
 
-    // creating a data type string
-  string name;
-//   console out the name variable
-    cout << "what is the name of the god?";
-    cin >> name;
-    cout << "the name of the god is " << name;
+//     // creating a data type string
+//   string name;
+// //   console out the name variable
+//     cout << "what is the name of the god?";
+//     cin >> name;
+//     cout << "the name of the god is " << name;
+
+// integral data types
+short a;
+int b;
+long c;
+long long d;
+// short <= int <= long <= long long
+unsigned short aa;
+unsigned int bb;
+unsigned long cc;
+unsigned long long dd;
+
+char x = 'A';
+cout << x << std::endl;
+
 }

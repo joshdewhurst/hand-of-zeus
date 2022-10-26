4062ac11b988af787f3652585706955d2f2904c4
joshdewhurst <joshuardewhurst@gmail.com>
2022-10-25 21:38:43 -0700

    started function to retreave passwords/usernames from a file

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+// including file stream to get info to and from the disk and the program
+
 using namespace std;
 // using the standard library for naming
 
 class loginZeus{
     // public keyword, meaning anyone can access
     public:
-        string usernameMatch;
-        string passwordMatch;
         loginZeus(){
             authenticate = false;
         };
@@ -16,30 +17,60 @@ class loginZeus{
         cout << "please enter your almighty name /nUsername:";
         // cin is input from user
         cin >> usernameMatch;
+
+        // retreaving the username from a file
+        username = getFile("users.txt");
+
         if(usernameMatch == username){
             cout << "please enter the almighty password:";
             cin >> passwordMatch;
+
+            password = getFile("password.txt");
+
             if(passwordMatch == password) {
                 cout << "correct password";
+                cin.get();
             }
             else {
                 cout << "wrong password";
+                login();
             }
         }
         else {
             cout << "wrong username";
+            login();
         }
     }
+
+    // define a function to retrieve the password and usernames from a stored file
+    string getFile(const char* p_file)
+        {
+            string line;
+            fstream file;
+
+            file.open(p_file, ios::in);
+            if(file.is_open())
+                {
+                    getline(file, line);
+                }
+            file.close();
+
+            return line;
+        }
+
     // only Zeus can access the following
      private:
         string password = "password1234";
         string username = "zeus@mtolympus.com";
+        string usernameMatch;
+        string passwordMatch;
         bool authenticate;
         
 };
 
-
 int main(){
     loginZeus loginZuesObject;
     loginZuesObject.login();
+
+    cin.get();
 }
